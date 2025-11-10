d79f676d33993c67328bd2c1631f8230cef60fbd
Kun Arief D. <koenarief@gmail.com>
2025-11-10 09:02:18 +0700

    apple icon

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,11 @@ export default defineConfig({
         theme_color: "#ffffff",
         icons: [
           {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
             src: "pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
@@ -26,7 +31,7 @@ export default defineConfig({
             type: "image/png",
           },
           {
-            src: "pwa-512x512-maskable.png",
+            src: "maskable-icon-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "maskable",
